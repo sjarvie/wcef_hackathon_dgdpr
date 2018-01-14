090d8d9709a5fd96b28e96607ff5c36af38b5e8f
Teddy Knox <eknox@linkedin.com>
2018-01-14 09:23:50 -0800

    Finish upload handler and add share handler.

--- proxy_service/handlers/upload_handler.py
+++ proxy_service/handlers/upload_handler.py
@@ -1,6 +1,5 @@
 import json
 import base64
-
 import ipdb
 import boto
 import requests
@@ -9,19 +8,16 @@ import tornado.web
 
 from boto.s3.key import Key
 
-
 CONN = boto.connect_s3()
 BUCKET = 'wcef-2018-dgdp'
 
-
-def encode_bytes_to_base_64_str(data):
-    encoded = base64.b64encode(data)
-    return bytes.decode(encoded)
-
-def decode_base64_str_into_bytes(s):
-    encoded = str.encode(s)
-    return base64.b64decode(encoded)
-
+# def encode_bytes_to_base_64_str(data):
+#     encoded = base64.b64encode(data)
+#     return bytes.decode(encoded)
+# 
+# def decode_base64_str_into_bytes(s):
+#     encoded = str.encode(s)
+#     return base64.b64decode(encoded)
 
 
 class UploadHandler(tornado.web.RequestHandler):
@@ -38,7 +34,6 @@ class UploadHandler(tornado.web.RequestHandler):
         """
         args = json.loads(self.request.body)
 
-
         # test an object can be created
         print('Got args {}'.format(args))
 
@@ -48,28 +43,21 @@ class UploadHandler(tornado.web.RequestHandler):
         sender_b64 = args['sender']
 
         # format is /public_key/fname
-        k_base = '/{}/{}'.format(sender_b64, fname)
+        k_base = '/{}/{}/ciphertext'.format(sender_b64, fname)
         bucket = CONN.get_bucket(BUCKET)
 
         # upload ciphertext
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         k_ciphertext = '{}/ciphertext'.format(k_base)
         upload_ciphertext = Key(bucket)
         upload_ciphertext.key = k_ciphertext
-        upload_key.set_contents_from_string(c_b64)
-
+        upload_ciphertext.set_contents_from_string(c_b64)
 
         # upload edek
-        import pdb; pdb.set_trace()
-        k_edek = '{}/ciphertext'.format(k_base)
+        # import pdb; pdb.set_trace()
+        k_edek = '{}/edek'.format(k_base)
         upload_edek = Key(bucket)
         upload_edek.key = k_ciphertext
-        upload_key.set_contents_from_string(edek_b64)
-
-
-
+        upload_edek.set_contents_from_string(edek_b64)
         self.write("OK")
-
-
-
 
