1b0afcf306912c9557ac087bf46b941647f09a3e
Abiy Seifu <abiyzel@gmail.com>
2018-01-14 00:49:53 -0800

    Use material table, use hash map DS

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Upload from './Upload';
 import UserList from './UserList'
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
 import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
 import Avatar from 'material-ui/Avatar';
-
-const routes = {
-  upload: <Upload />,
-  list: <UserList/>
-};
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      current_page: 'upload'
-    };
-  }
-
-  setPage(pageName) {
-    this.setState({
-      current_page: pageName
-    });
   }
 
   render() {
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const DFDPR = () => {
   return (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
       <App/>
     </MuiThemeProvider>
   );
