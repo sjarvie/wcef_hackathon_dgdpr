f9e1ce3e32fc5376db4dff11a78642b655514335
Abiy Seifu <abiyzel@gmail.com>
2018-01-14 09:37:19 -0800

    remove dark theme modules

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const DFDPR = () => {
   return (
-    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <MuiThemeProvider>
       <App/>
     </MuiThemeProvider>
   );
